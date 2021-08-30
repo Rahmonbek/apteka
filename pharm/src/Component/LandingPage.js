5e268e962047c4ab332a9ac8a4b6c0771598d3e9
Murod <74758823+MurodjohnKhayitov@users.noreply.github.com>
2021-08-31 01:11:08 +0500

    Soiwnrffwrl
    fwernf'pwnirf

--- pharm/src/Component/LandingPage.js
+++ pharm/src/Component/LandingPage.js
@@ -3,21 +3,9 @@ import { Container } from "react-bootstrap";
 import styles from "./Css/LandingPage.module.css";
 import CardGroup from "./CardGroup";
 import Carousell from "./Carousel/Carousel";
-import Doctor1 from "./img/Rectangle 10.png";
-import Doctor2 from "./img/Rectangle 11.png";
-import Doctor3 from "./img/Rectangle 8.png";
-import Doctor4 from "./img/Rectangle 9.png";
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  PhoneOutlined,
-  YoutubeOutlined,
-} from "@ant-design/icons";
+import ServiceShow from "./Service/ServiceShow";
 import AOS from "aos";
-
-import { Card } from "antd";
 import Menu from "./Menu";
-const { Meta } = Card;
 
 export default function LandingPage() {
   AOS.init({
@@ -28,119 +16,8 @@ export default function LandingPage() {
       <Menu />
       <Container>
         <section id="service"></section>
-        <div className={styles.ServiceMenu}>
-          <div className={styles.ServiceMenuText}>
-            <p className={styles.ServiceMenuText1}>
-              Bizning Eng Yaxshi Xizmatlarimiz
-            </p>
-            <p className={styles.ServiceMenuText2}>
-              Semper integer est quis leo, nunc. Quis lectus amet aenean massa
-              turpis senectus pulvinar. Pharetra gravida volutpat faucibus
-              faucibus. Quis lectus amet aenean massa turpis senectus pulvinar.
-              Pharetra gravida volutpat faucibus faucibus.
-            </p>
-          </div>
-          <div className={styles.ServiceMenuCard}>
-            <div className={styles.ServiceMenuCard1}>
-              {/* Card Group */}
-              <div
-                data-aos="fade-up"
-                data-aos-anchor-placement="center-bottom"
-                className={styles.Card}
-              >
-                <div className={styles.CardIcons}>
-                  <i class="fas fa-ambulance fa-2x"></i>
-                </div>
-                <div className={styles.CardText} style={{ width: "60%" }}>
-                  <p className={styles.CardTextBold}>Afzalliklar</p>
-                  <p className={styles.CardTextSmall}>
-                    Siz uyingizdan chiqmagan holda dori buyurtma berasiz va biz
-                    yetkazib beramiz.
-                  </p>
-                </div>
-              </div>
-              <div
-                data-aos="fade-up"
-                data-aos-anchor-placement="center-bottom"
-                className={styles.Card}
-              >
-                <div className={styles.CardIcons}>
-                  <i class="fas fa-user-md fa-2x"></i>
-                </div>
-                <div className={styles.CardText} style={{ width: "60%" }}>
-                  <p className={styles.CardTextBold}>Qulayliklari</p>
-                  <p className={styles.CardTextSmall}>
-                    Siz bizning Malakali doctorlarimizdan istalgan vaqt maslahat
-                    olishingiz mumkin.
-                  </p>
-                </div>
-              </div>
-              <div
-                data-aos="fade-up"
-                data-aos-anchor-placement="center-bottom"
-                className={styles.Card}
-              >
-                <div className={styles.CardIcons}>
-                  <i class="fas fa-book-medical fa-2x "></i>
-                </div>
-                <div className={styles.CardText} style={{ width: "60%" }}>
-                  <p className={styles.CardTextBold}>Yaxshi G'amxo'rlik</p>
-                  <p className={styles.CardTextSmall}>
-                    Agar sizning imkoniyatingiz cheklangan bo'lsa biz sizga
-                    yordam beramiz.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.ServiceMenuCard1}>
-              {/* Card Group */}
-              <div
-                data-aos="fade-up"
-                className={styles.Card}
-              >
-                <div className={styles.CardIcons}>
-                  <i class="fas fa-headset fa-2x"></i>
-                </div>
-                <div className={styles.CardText} style={{ width: "60%" }}>
-                  <p className={styles.CardTextBold}>Favqulodda Yordam</p>
-                  <p className={styles.CardTextSmall}>
-                    Sizga Favqulodda tezkor yordam kerakmi, unda bizga murojaat
-                    qiling.
-                  </p>
-                </div>
-              </div>
-              <div
-                data-aos="fade-up"
-                className={styles.Card}
-              >
-                <div className={styles.CardIcons}>
-                  <i class="fas fa-book-medical fa-2x "></i>
-                </div>
-                <div className={styles.CardText} style={{ width: "60%" }}>
-                  <p className={styles.CardTextBold}>Ma'lumot uchun</p>
-                  <p className={styles.CardTextSmall}>
-                    Siz kerakli dorilaringiz haqida ba'tafsil shu yerda ma'lumot
-                    olishingiz mumkin.
-                  </p>
-                </div>
-              </div>
-              <div
-                data-aos="fade-up"
-                className={styles.Card}
-              >
-                <div className={styles.CardIcons}>
-                  <i class="fas fa-user-clock fa-2x"></i>
-                </div>
-                <div className={styles.CardText} style={{ width: "60%" }}>
-                  <p className={styles.CardTextBold}>24 Soatlik Xizmat</p>
-                  <p className={styles.CardTextSmall}>
-                    Bizga sizga 24/7 xizmat ko'rsatamiz. Siz biz bilan xoxlagan
-                    vaqtingiz bog'lanishingiz mumkin.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div>
+          <ServiceShow />
         </div>
         <section id="List"></section>
         <div>
---
+++ pharm/src/Component/Service/ServiceShow.js
@@ -0,0 +1,263 @@
+import React from "react";
+import styles from "../Css/ServiceShow.module.css";
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import Pagination from "./ServiceGroup.js/Pagination_1";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AOS from "aos";
+
+import {
+  faHeadset,
+  faBook,
+  faCocktail,
+  faFax,
+  faHospital,
+  faMedkit,
+} from "@fortawesome/free-solid-svg-icons";
+export default function ServiceShow() {
+  const [posts, setPosts] = useState([
+    {
+      userId: 1,
+      id: 1,
+      icon: <FontAwesomeIcon icon={faHospital} />,
+      title: "Afzalliklari",
+      body: "Siz uyingizdan chiqamagan holda dori buyurtma berasiz. Biz yetkazib beramiz",
+    },
+    {
+      userId: 1,
+      id: 2,
+      icon: <FontAwesomeIcon icon={faFax} />,
+      title: "Qulayliklar",
+      body: "Siz bizning malakali Doctorlarimizdan istalgan vaqtda, maslahat olishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 3,
+      icon: <FontAwesomeIcon icon={faCocktail} />,
+
+      title: "Yaxshi G'amxurlik",
+      body: "Agar sizning imkoniyatingiz cheklangan bo'lsa, biz sizga yordam beramiz.",
+    },
+    {
+      userId: 1,
+      id: 4,
+      icon: <FontAwesomeIcon icon={faHeadset} />,
+
+      title: "Favqulodda Yordam",
+      body: "Sizga Favqulodda tezkor yordam kerakmi, unda bizga murojaat qiling.",
+    },
+    {
+      userId: 1,
+      id: 5,
+      icon: <FontAwesomeIcon icon={faBook} />,
+
+      title: "Ma'lumot Uchun",
+      body: "Sizga kerakli dorilaringiz xaqida  shu yerda ba'tafsil shu yerda ma'lumot olishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 6,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 7,
+      icon: <FontAwesomeIcon icon={faHospital} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 8,
+      icon: <FontAwesomeIcon icon={faHospital} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 9,
+      icon: <FontAwesomeIcon icon={faHospital} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 10,
+      icon: <FontAwesomeIcon icon={faHospital} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 11,
+      icon: <FontAwesomeIcon icon={faHospital} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 12,
+      icon: <FontAwesomeIcon icon={faHospital} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 13,
+      icon: <FontAwesomeIcon icon={faHospital} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 14,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 15,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 16,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 17,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 18,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 19,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 20,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 21,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 22,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 23,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+    {
+      userId: 1,
+      id: 24,
+      icon: <FontAwesomeIcon icon={faMedkit} />,
+
+      title: "24 Soatlik Xizmat ",
+      body: "Biz mijozlarga 24/7 xizmat ko'rsatamiz. Siz bizga isqalgan vaqtda murojaat qilishingiz mumkin.",
+    },
+  ]);
+  const [showPerPage, setShowPerPage] = useState(6);
+  const [pagination, setPagination] = useState({
+    start: 0,
+    end: showPerPage,
+  });
+
+  const onPaginationChange = (start, end) => {
+    setPagination({ start: start, end: end });
+  };
+  AOS.init({
+    duration: 1000,
+  });
+  return (
+    <div className={styles.Service}>
+      <Container>
+        <div>
+          <div className={styles.ServiceMenuText}>
+            <p className={styles.ServiceMenuText1}>
+              Bizning Eng Yaxshi Xizmatlarimiz
+            </p>
+            <p className={styles.ServiceMenuText2}>
+              Semper integer est quis leo, nunc. Quis lectus amet aenean massa
+              turpis senectus pulvinar. Pharetra gravida volutpat faucibus
+              faucibus. Quis lectus amet aenean massa turpis senectus pulvinar.
+              Pharetra gravida volutpat faucibus faucibus.
+            </p>
+          </div>
+          <div className={styles.ServiceMenu}>
+            {posts.slice(pagination.start, pagination.end).map((post) => (
+              <div
+                data-aos="zoom-in-up"
+                className={styles.ServiceMenuCard}
+                key={post.id}
+              >
+                <div className={styles.CardIcons}>
+                  <i>{post.icon}</i>
+                </div>
+                <div className={styles.CardText} style={{ width: "60%" }}>
+                  <p className={styles.CardTextBold}>{post.title}</p>
+                  <p className={styles.CardTextSmall}>{post.body}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <Pagination
+            showPerPage={showPerPage}
+            onPaginationChange={onPaginationChange}
+            total={posts.length}
+          />
+        </div>
+      </Container>
+    </div>
+  );
+}
