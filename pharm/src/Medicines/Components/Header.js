8553aad322f0ca2aee31c86caeedea33473e4c54
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-09-09 00:42:30 +0500

    lmkjsdsfd

--- pharm/src/Medicines/Components/Header.js
+++ pharm/src/Medicines/Components/Header.js
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Rasm from "./../../Component/img/Brand.png";
-import { Navbar, Container, Nav, Image } from "react-bootstrap";
+import { Navbar, Container, Nav, Image, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 function Header() {
+  const [res, setRes] = useState(true);
+
   return (
-    <div className="dorilar_header">
-      <Navbar className="dorilar-navbar">
-        <Container>
-          <Navbar.Brand>
-            <Image src={Rasm} />
-          </Navbar.Brand>
-          <Nav className="dorilar-nav">
+    <>
+      {res ? (
+        <div className="dorilar-res-menu">
+          <div className="menu-dorilar">
+            <Button onClick={() => setRes(!res)}>
+              <AiOutlineClose />
+            </Button>
+          </div>
+          <div className="dorilar-navbar">
             <Nav.Link>
               <Link to="/">Bosh sahifa</Link>
             </Nav.Link>
             <Nav.Link href="#dorilar">
               <span>Dorilar</span>
             </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <div className="dorilar-main"></div>
-    </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="dorilar_header">
+        <Navbar className="dorilar-navbar">
+          <Container>
+            <Navbar.Brand>
+              <Image src={Rasm} />
+            </Navbar.Brand>
+            <div className="menu-dorilar">
+              <Button onClick={() => setRes(!res)}>
+                <AiOutlineMenu />
+              </Button>
+            </div>
+            <Nav className={`dorilar-nav active`}>
+              <Nav.Link>
+                <Link to="/">Bosh sahifa</Link>
+              </Nav.Link>
+              <Nav.Link href="#dorilar">
+                <span>Dorilar</span>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <div className="dorilar-main"></div>
+      </div>
+    </>
   );
 }
 
