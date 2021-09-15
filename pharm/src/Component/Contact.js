dee47a078e409b5cc7e740833edff9e2c233813e
KarshiyevaNilufar <77773971+KarshiyevaNilufar@users.noreply.github.com>
2021-09-15 15:47:02 +0500

    t

--- pharm/src/Component/Contact.js
+++ pharm/src/Component/Contact.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import styles from './Css/contact.module.css'
 import contact1 from './img/contact1-removebg-preview.png'
 import { Container, Row, Col } from "react-bootstrap";
-import { YMaps, Map } from 'react-yandex-maps';
+import { YMaps, Map ,Clusterer, Placemark, 
+  TypeSelector, ZoomControl, GeolocationControl, RouteButton, TrafficControl} from 'react-yandex-maps';
 import Rasm from "./img/Brand.png";
 import { Navbar,Nav, Image } from "react-bootstrap";
 import { MdTimer } from "react-icons/md";
@@ -32,7 +33,35 @@ const tailLayout = {
   },
 };
 export default class Contact extends Component {
-  state = {width: '100%', height: '100%'};
+  state = {
+    points:[
+      {
+          name: "ZAKO",
+          param: [41.354353, 69.223353], 
+        },
+        {
+          name: "IT Tower",
+          param: [41.317648, 69.230585], 
+        },
+        {
+          name: "PDP",
+          param: [41.295995, 69.175190], 
+        },
+        {
+          name: "ICT academy",
+          param: [41.281412, 69.212667], 
+        },
+        {
+          name: "Roboticslab",
+          param: [41.235069, 69.247354], 
+        },
+        {
+          name: "Najot ta'lim",
+          param: [41.286009, 69.253749], 
+        },
+       
+  ]
+  };
     formRef = React.createRef();
   onGenderChange = (value) => {
     switch (value) {
@@ -159,11 +188,37 @@ export default class Contact extends Component {
             </Col>
             <Col lg={6}>
               <h2 style={{fontFamily: 'Arial, Helvetica, sans-serif',marginTop:'50px'}}>Manzilimiz:</h2>
-              <YMaps style={{width:'100%'}}>
-    <div className={styles.mapY} style={{boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',width:'550px'}}>
-      <Map width={550} height={400} defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
-    </div>
-  </YMaps>
+              <YMaps >
+        <Map
+          width='100vw'
+          height='100vh'
+          defaultState={{
+            center: [41.311151, 69.279716],
+            zoom: 8
+          }}
+        >
+          <Clusterer options={{  preset: 'islands#invertedVioletClusterIcons',  groupByCoordinates: false, }}  >
+            {this.state.points.map((coordinates, index) => (
+              
+              <Placemark  balloonContent= '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />'
+              iconContent= {coordinates.name}   key={index}
+               geometry={coordinates.param} 
+               options={{preset: "islands#blueStretchyIcon",
+               // Отключаем кнопку закрытия балуна.
+               balloonCloseButton: false,
+                // Балун будем открывать и закрывать кликом по иконке метки.
+               hideIconOnBalloonOpen: false,
+               openBalloonOnClick:true}}/>
+            ))}
+          </Clusterer> 
+          <GeolocationControl options={{ float: 'left' }} />
+          <TypeSelector options={{ float: 'right' }} />
+          <TrafficControl options={{ float: 'right' }} />
+          <RouteButton options={{ float: 'right' }} />
+          <ZoomControl options={{ float: 'left' }} />
+  
+        </Map>
+    </YMaps>
             </Col>
 
           </Row>
@@ -214,7 +269,7 @@ export default class Contact extends Component {
                   </Col>
               </Row>
           </Container>
-          <svg className={styles.wave}  id="wave" style={{transform:'rotate(180deg)', transition: '0.3s',height:'300px',width:'100%',overflow:'hidden'}} viewBox="0 0 1440 490" version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0"><stop stop-color="rgba(28, 72, 122, 1)" offset="0%"></stop><stop stop-color="rgba(28, 72, 122, 1)" offset="100%"></stop></linearGradient></defs><path style={{transform:'translate(0, 0px)', opacity:'1'}} fill="url(#sw-gradient-0)" d="M0,0L34.3,40.8C68.6,82,137,163,206,171.5C274.3,180,343,114,411,89.8C480,65,549,82,617,130.7C685.7,180,754,261,823,318.5C891.4,376,960,408,1029,367.5C1097.1,327,1166,212,1234,196C1302.9,180,1371,261,1440,294C1508.6,327,1577,310,1646,294C1714.3,278,1783,261,1851,269.5C1920,278,1989,310,2057,318.5C2125.7,327,2194,310,2263,310.3C2331.4,310,2400,327,2469,326.7C2537.1,327,2606,310,2674,318.5C2742.9,327,2811,359,2880,375.7C2948.6,392,3017,392,3086,392C3154.3,392,3223,392,3291,359.3C3360,327,3429,261,3497,220.5C3565.7,180,3634,163,3703,130.7C3771.4,98,3840,49,3909,57.2C3977.1,65,4046,131,4114,204.2C4182.9,278,4251,359,4320,359.3C4388.6,359,4457,278,4526,228.7C4594.3,180,4663,163,4731,130.7C4800,98,4869,49,4903,24.5L4937.1,0L4937.1,490L4902.9,490C4868.6,490,4800,490,4731,490C4662.9,490,4594,490,4526,490C4457.1,490,4389,490,4320,490C4251.4,490,4183,490,4114,490C4045.7,490,3977,490,3909,490C3840,490,3771,490,3703,490C3634.3,490,3566,490,3497,490C3428.6,490,3360,490,3291,490C3222.9,490,3154,490,3086,490C3017.1,490,2949,490,2880,490C2811.4,490,2743,490,2674,490C2605.7,490,2537,490,2469,490C2400,490,2331,490,2263,490C2194.3,490,2126,490,2057,490C1988.6,490,1920,490,1851,490C1782.9,490,1714,490,1646,490C1577.1,490,1509,490,1440,490C1371.4,490,1303,490,1234,490C1165.7,490,1097,490,1029,490C960,490,891,490,823,490C754.3,490,686,490,617,490C548.6,490,480,490,411,490C342.9,490,274,490,206,490C137.1,490,69,490,34,490L0,490Z"></path></svg>
+          
       </div>
       <div style={{position:'relative'}}>
       <div className={styles.aloqa}>
