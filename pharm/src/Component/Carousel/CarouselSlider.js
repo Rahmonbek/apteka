4222f82be0cece747f1d9325e50ddd154d54f04a
Murod <74758823+MurodjohnKhayitov@users.noreply.github.com>
2021-09-27 16:48:19 +0500

    sw
    sw

--- pharm/src/Component/Carousel/CarouselSlider.js
+++ pharm/src/Component/Carousel/CarouselSlider.js
@@ -24,14 +24,15 @@ export default function CarouselSlider() {
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 700 },
+      breakpoint: { max: 1024, min: 768 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 700, min: 0 },
+      breakpoint: { max: 767, min: 0 },
       items: 1,
     },
   };
+
   const x = "#";
 
   return (
@@ -69,14 +70,11 @@ export default function CarouselSlider() {
               </path>
             </svg>
             <div
-              style={{ paddingTop: "10px" }}
+              // style={{ paddingTop: "10px" }}
               className={style.CardFooterText1}
             >
-              <p style={{ marginTop: "8px" }}>Dori-Darmon</p>
-              <p style={{ marginTop: "-6px" }}>
-                {" "}
-                Toshkent, Yunusobod tumani, 2-kvartal, 7B
-              </p>
+              <p>Dori-Darmon</p>
+              <p> Toshkent, Yunusobod tumani, 2-kvartal, 7B</p>
             </div>{" "}
             <div className={style.social_icons_Item}>
               <ul
@@ -127,13 +125,14 @@ export default function CarouselSlider() {
               ></path>
             </svg>
             <div
-              style={{ paddingTop: "10px" }}
+              // style={{ paddingTop: "10px" }}
               className={style.CardFooterText1}
             >
-              <p style={{ marginTop: "8px" }}>Dori-Darmon</p>
-              <p style={{ marginTop: "-6px" }}>
-                {" "}
-                Toshkent, Yunusobod tumani, 2-kvartal, 7B
+              {/* style={{ marginTop: "8px" }} bu dori darmonniki */}
+              <p>Dori-Darmon</p>
+              <p>
+                {/* style={{ marginTop: "-6px" }}  bu adressniki*/} Toshkent,
+                Yunusobod tumani, 2-kvartal, 7B
               </p>
             </div>{" "}
             <div className={style.social_icons_Item}>
@@ -186,14 +185,11 @@ export default function CarouselSlider() {
               ></path>
             </svg>
             <div
-              style={{ paddingTop: "10px" }}
+              // style={{ paddingTop: "10px" }}
               className={style.CardFooterText1}
             >
-              <p style={{ marginTop: "8px" }}>Dori-Darmon</p>
-              <p style={{ marginTop: "-6px" }}>
-                {" "}
-                Toshkent, Yunusobod tumani, 2-kvartal, 7B
-              </p>
+              <p>Dori-Darmon</p>
+              <p> Toshkent, Yunusobod tumani, 2-kvartal, 7B</p>
             </div>{" "}
             <div className={style.social_icons_Item}>
               <ul
@@ -249,14 +245,11 @@ export default function CarouselSlider() {
               </i>
             </div>
             <div
-              style={{ paddingTop: "10px" }}
+              // style={{ paddingTop: "10px" }}
               className={style.CardFooterText1}
             >
-              <p style={{ marginTop: "8px" }}>Dori-Darmon</p>
-              <p style={{ marginTop: "-6px" }}>
-                {" "}
-                Toshkent, Yunusobod tumani, 2-kvartal, 7B
-              </p>
+              <p>Dori-Darmon</p>
+              <p> Toshkent, Yunusobod tumani, 2-kvartal, 7B</p>
             </div>{" "}
             <div className={style.social_icons_Item}>
               <ul
@@ -306,13 +299,13 @@ export default function CarouselSlider() {
                 d="M0,160L48,170.7C96,181,192,203,288,181.3C384,160,480,96,576,64C672,32,768,32,864,53.3C960,75,1056,117,1152,122.7C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
               ></path>
             </svg>
-            <div style={{ padding: "10px" }} className={style.CardFooterText1}>
-              <p style={{ marginTop: "8px" }}>Dori-Darmon</p>
-              <p style={{ marginTop: "-6px" }}>
+            <div  className={style.CardFooterText1}>
+              <p >Dori-Darmon</p>
+              <p >
                 {" "}
                 Toshkent, Yunusobod tumani, 2-kvartal, 7B
               </p>
-            </div>{" "}
+            </div>
             <div className={style.social_icons_Item}>
               <ul
                 style={{ listStyleType: "none", textDecoration: "none" }}
